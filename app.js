84af846bd24d8043908cc7036e85f7f3f1a4ef85
Wenyan Lin <wennyan.lin@gmail.com>
2023-02-21 10:24:10 +0100

    Initial Express commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const cors = require('cors');  // add at the top
+
+var app = express();
+
+app.use(cors());  // add after 'app' is created
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
