7ffeacbe623297f2edb989e0552b74845945d21d
Wenyan Lin <wennyan.lin@gmail.com>
2023-02-24 22:22:47 +0100

    Updated GET ALL and GET by ID query

--- model/database.js
+++ model/database.js
@@ -22,7 +22,7 @@ con.connect(function (err) {
   let sql = fs.readFileSync(__dirname + "/init_db.sql").toString();
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table creation `students` was successful!");
+    console.log("Table creation `moodboards` was successful!");
 
     console.log("Closing...");
   });
--- routes/moodboards.js
+++ routes/moodboards.js
@@ -4,32 +4,50 @@ const db = require("../model/helper");
 
 /* GET users listing. */
 router.get("/", async (req, res, next) => {
-  const results = await db("SELECT * FROM moodboards");
-  res.send(results.data);
+  try {
+    const results = await db(
+      "SELECT m.*, e.* FROM moodboards AS m RIGHT JOIN elements AS e ON m.id = e.boardId;"
+    );
+    res.send(results.data);
+  } catch (error) {
+    res.status(500).send({ error: err.message });
+  }
 });
 
 router.get("/:id", async (req, res, next) => {
   const moodboardId = req.params.id;
   try {
     const results = await db(
-      `SELECT * FROM moodboards WHERE id = ${moodboardId}`
+      `SELECT m.*, e.* FROM moodboards AS m RIGHT JOIN elements AS e ON m.id = e.boardId WHERE m.id = ${moodboardId}`
     );
-    if (results.data.length === 1) {
-      res.send(results.data[0]);
+    console.log(results.data);
+    if (results.data.length >= 1) {
+      res.send(results.data);
     } else {
       res.status(404).send("Moodboard not found.");
     }
-  } catch (error) {
+  } catch (err) {
     res.status(500).send(err);
   }
 });
 
 router.post("/", async (req, res) => {
-  const { name } = req.body;
+  const { name, elements } = req.body;
+
   try {
+    //creating a new board
+    await db(`INSERT INTO moodboards (name) VALUES ('${name}')`);
+    //getting the id of the new board
     const results = await db(
-      `INSERT INTO moodboards (name) VALUES ('${name}')`
+      `SELECT id FROM moodboards ORDER BY id DESC LIMIT 1`
     );
+    const boardId = results.data[0].id;
+
+    for (const element of elements) {
+      await db(
+        `INSERT INTO elements (boardId, value, type) VALUE (${boardId}, ${element.value}, ${element.type})`
+      );
+    }
     res.send(results.data);
   } catch (error) {
     res.status(500).send({ error: err.message });
